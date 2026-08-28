dbc1c5f822ac178f285baf48f1a318695de11f22
NeoTrix <dolpinsy@gmail.com>
2026-08-28 17:09:54 +0800

    Delete src-tauri directory

--- src-tauri/main.rs
+++
@@ -1,8 +0,0 @@
-// Tauri 2.0 主入口 - 最简版，只启动窗口加载前端
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-fn main() {
-    tauri::Builder::default()
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
-}
